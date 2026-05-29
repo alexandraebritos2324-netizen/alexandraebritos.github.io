4fbf5aba37e10967e45c3a325d366585c43deb9e
alexandraebritos2324-netizen <alexandraebritos2324@gmail.com>
2026-05-29 14:39:05 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.details-toggle').forEach((button) => {
-  button.addEventListener('click', () => {
-    const details = button.nextElementSibling;
-    details.classList.toggle('open');
-    button.textContent = details.classList.contains('open') ? 'Show less' : 'Read more';
-  });
-});
